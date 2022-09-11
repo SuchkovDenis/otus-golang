50c04f5b50ceda9462d37239afe610b0d7553cad
suchkovdenis <ny1828@yandex.ru>
2022-09-11 16:28:07 +0300

    HW2 is completed

--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -9,18 +9,55 @@ import (
 
 func TestUnpack(t *testing.T) {
 	tests := []struct {
+		desc     string
 		input    string
 		expected string
 	}{
-		{input: "a4bc2d5e", expected: "aaaabccddddde"},
-		{input: "abccd", expected: "abccd"},
-		{input: "", expected: ""},
-		{input: "aaa0b", expected: "aab"},
-		// uncomment if task with asterisk completed
-		// {input: `qwe\4\5`, expected: `qwe45`},
-		// {input: `qwe\45`, expected: `qwe44444`},
-		// {input: `qwe\\5`, expected: `qwe\\\\\`},
-		// {input: `qwe\\\3`, expected: `qwe\3`},
+		{
+			desc:     "Case-1: Base case with numbers",
+			input:    "a4bc2d5e",
+			expected: "aaaabccddddde",
+		},
+		{
+			desc:     "Case-2: Base case without numbers",
+			input:    "abccd",
+			expected: "abccd",
+		},
+		{
+			desc:     "Case-3: Empty string",
+			input:    "",
+			expected: "",
+		},
+		{
+			desc:     "Case-4: Zero number in string",
+			input:    "aaa0b",
+			expected: "aab",
+		},
+		{
+			desc:     "Case-5: Zero number to empty string",
+			input:    "a0",
+			expected: "",
+		},
+		{
+			desc:     "Case-6: Complex bytes runes in string without numbers",
+			input:    "你好世界",
+			expected: "你好世界",
+		},
+		{
+			desc:     "Case-7: Complex bytes runes in string with numbers",
+			input:    "你1好2世0界",
+			expected: "你好好界",
+		},
+		{
+			desc:     "Case-8: Special characters in string",
+			input:    "d\n5abc",
+			expected: "d\n\n\n\n\nabc",
+		},
+		{
+			desc:     "Case-9: Symbol characters string",
+			input:    "-5$@#3~'1`2",
+			expected: "-----$@###~'``",
+		},
 	}
 
 	for _, tc := range tests {
@@ -34,7 +71,7 @@ func TestUnpack(t *testing.T) {
 }
 
 func TestUnpackInvalidString(t *testing.T) {
-	invalidStrings := []string{"3abc", "45", "aaa10b"}
+	invalidStrings := []string{"3abc", "45", "aaa10b", "0"}
 	for _, tc := range invalidStrings {
 		tc := tc
 		t.Run(tc, func(t *testing.T) {
